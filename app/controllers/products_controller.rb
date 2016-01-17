5c9b2c23f0d5312c4fe9e1750956c4bde19b52f3
TIJO THOMAS <tt.opt.ins@gmail.com>
2016-01-17 16:54:45 -0600

    Added REST routes and ability to add, delete, edit, and update new products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,4 +4,36 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find_by(id: params[:id])
+  end
+
+  def new
+
+  end
+
+  def create
+    @product = Product.create({name: params[:name], price: params[:price], image: params[:image], description: params[:description] })
+  end
+
+  def edit
+    @product = Product.find_by(id: params[:id])
+    #above is same as: @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+
+    @product.update({name: params[:name], price: params[:price], image: params[:image], description: params[:description] })
+  end
+
+  def destroy
+    @product = Product.find_by(id: params[:id])
+    @product.destroy
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  get'/stuff' => 'products#item_display'
+  get '/' => 'products#index'
+  get '/products' => 'products#item_display'
+
+  get '/products/new' => 'products#new' # look at 'photos' example in rails guide Josh sent out on Jan 17th.
+  post '/products' => 'products#create'
+
+  get '/products/:id' => 'products#show'
+
+  get '/products/:id/edit' => 'products#edit'
+  patch '/products/:id' => 'products#update'
+
+  delete '/products/:id' => 'products#destroy'
 end
